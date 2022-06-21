33fdd5e22c438774949e97719310b920439a4af3
AndriyShkrobut <andrii.shkrobut@gmail.com>
2022-06-21 04:42:23 +0300

    fix(kino24-like): formatting and minor fixes

--- kino24-like/kino24-like/Startup.cs
+++ kino24-like/kino24-like/Startup.cs
@@ -11,31 +11,35 @@ namespace kino24_like
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
             Configuration["Jwt:Key"] = Environment.GetEnvironmentVariable("JWT_SECRET");
 
             var host = Environment.GetEnvironmentVariable("HOST");
             var password = Environment.GetEnvironmentVariable("PASSWORD");
-            var dbconnection = "Host=" + host + ";" + Configuration["ConnectionStrings:FeedbackServiceDBConnection"] + "Password=" + password + ";";
+            var dbconnection = "Host=" + host + ";" + Configuration["ConnectionStrings:FeedbackServiceDBConnection"] +
+                               "Password=" + password + ";";
             Configuration["ConnectionStrings:FeedbackServiceDBConnection"] = dbconnection;
         }
 
         public IConfiguration Configuration { get; }
 
-        public void Configure(IApplicationBuilder app,
-                              IWebHostEnvironment env,
-                              IServiceProvider serviceProvider)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/V1/swagger.json", "kino24-like");
-                                    c.RoutePrefix = string.Empty;
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/V1/swagger.json", "kino24-like");
+                c.RoutePrefix = string.Empty;
             });
-            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             else
             {
                 app.ConfigureCustomExceptionMiddleware();
                 app.UseHsts();
             }
+
             app.UseWebSockets();
             app.UseStatusCodePages();
             app.UseHttpsRedirection();
@@ -43,8 +47,8 @@ namespace kino24_like
             app.UseCors(builder =>
             {
                 builder.AllowAnyMethod()
-                       .AllowAnyHeader()
-                       .AllowAnyOrigin();
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin();
             });
             app.UseAuthentication();
             app.UseAuthorization();
